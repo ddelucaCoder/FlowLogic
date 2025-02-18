b941b510a29faae77a70f2c0a28333f37130ac4d
ddelucaCoder <djdeluca@purdue.edu>
2025-02-18 09:21:13 -0500

    Added basic constructors to grid class and added GridObjects interface

---
+++ src/GridObjects.java
@@ -0,0 +1,12 @@
+public interface GridObjects {
+    private int rowNum;
+    private int colNum;
+
+    public int getRowNum() {
+        return rowNum;
+    }
+
+    public int getColNum() {
+        return colNum;
+    }
+}
