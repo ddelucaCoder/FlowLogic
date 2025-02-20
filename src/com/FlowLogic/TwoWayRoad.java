010a05274d234cb6b22665afc23ba94f8dcbcf53
ihallman <ihallman@purdue.edu>
2025-02-20 10:10:00 -0500

    implemented twowayroad class

---
+++ src/com/FlowLogic/TwoWayRoad.java
@@ -0,0 +1,29 @@
+package com.FlowLogic;
+
+public class TwoWayRoad extends Road {
+
+    private OneWayRoad left;
+    private OneWayRoad right;
+
+    public TwoWayRoad(int orientation, int speedLimit, boolean isInRoad, int inCars, int rowNum, int colNum, OneWayRoad left, OneWayRoad right) {
+        super(orientation, speedLimit, isInRoad, inCars, rowNum, colNum);
+        this.left = left;
+        this.right = right;
+    }
+
+    public OneWayRoad getLeft() {
+        return left;
+    }
+
+    public void setLeft(OneWayRoad left) {
+        this.left = left;
+    }
+
+    public OneWayRoad getRight() {
+        return right;
+    }
+
+    public void setRight(OneWayRoad right) {
+        this.right = right;
+    }
+}
