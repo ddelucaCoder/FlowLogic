f80b5120e793692dc3518c7afae3ceec0cbfef81
ddelucaCoder <djdeluca@purdue.edu>
2025-02-25 08:47:28 -0500

    Tested and fixed graph to grid and grid to graph algorithms

--- src/testing/GridTest.java
+++ src/testing/GridTest.java
@@ -4,7 +4,6 @@ import com.FlowLogic.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -16,6 +15,7 @@ public class GridTest {
     @BeforeEach
     void setUp() {
         grid = new Grid(numRows, numCols);
+        Grid.GRID_SIZE = 32;
     }
 
     @Test
@@ -70,5 +70,65 @@ public class GridTest {
 
         assertEquals(grid.getAtSpot(row, col), obj1);
         
+    }
+
+    @Test
+    void testGridToCoordinate() {
+        // FOUR CORNERS FIRST
+        // grid spot (0, 0) = (0, 288) TOP LEFT
+        int[] response = {0, 288};
+        assertArrayEquals(grid.gridToCoordinate(0, 0), response);
+
+        // grid spot (9, 0) = (0, 0) BOTTOM LEFT
+        response[0] = 0;
+        response[1] = 0;
+        assertArrayEquals(grid.gridToCoordinate(9, 0), response);
+
+        // grid spot (9, 9) = (288, 288) BOTTOM RIGHT
+        response[0] = 288;
+        response[1] = 0;
+        assertArrayEquals(grid.gridToCoordinate(9, 9), response);
+
+        // grid spot (0, 9) = (288, 288) TOP RIGHT
+        response[0] = 288;
+        response[1] = 288;
+        assertArrayEquals(grid.gridToCoordinate(0, 9), response);
+
+        // NOW SOME RANDOM ONES
+
+        // grid spot (8, 1) = (32, 32) TOP RIGHT
+        response[0] = 32;
+        response[1] = 32;
+        assertArrayEquals(grid.gridToCoordinate(8, 1), response);
+
+        // grid spot (6, 3) = (96, 96) TOP RIGHT
+        response[0] = 96;
+        response[1] = 96;
+        assertArrayEquals(grid.gridToCoordinate(6, 3), response);
+
+        // grid spot (6, 4) = (128, 96) TOP RIGHT
+        response[0] = 128;
+        response[1] = 96;
+        assertArrayEquals(grid.gridToCoordinate(6, 4), response);
+    }
+
+    @Test
+    void testCoordinateToGrid() {
+        int[] response = {0, 0};
+        assertArrayEquals(grid.coordinateToGrid(4, 296), response);
+
+
+        response[0] = 6;
+        response[1] = 3;
+        assertArrayEquals(grid.coordinateToGrid(100, 100), response);
+
+        response[0] = 0;
+        response[1] = 9;
+        assertArrayEquals(grid.coordinateToGrid(300, 300), response);
+
+        response[0] = 9;
+        response[1] = 9;
+        assertArrayEquals(grid.coordinateToGrid(300, 10), response);
+
     }
 }
