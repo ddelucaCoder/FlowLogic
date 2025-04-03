176158784b0fe24513cd5cf6f047b33385ca540e
ddelucaCoder <djdeluca@purdue.edu>
2025-04-03 13:41:49 -0400

    Corrected Vehicle and added test

--- src/testing/BasicSimTest.java
+++ src/testing/BasicSimTest.java
@@ -43,8 +43,32 @@ public class BasicSimTest {
             TrafficController tc = new TrafficController(1, g);
 
             // run the sim
-            tc.runSimulation();
-            // print the output
+            Simulation sim = tc.runSimulation();
+
+            Grid h = new Grid(5, 5);
+            h.addObject(new OneWayRoad(Orientation.HORIZONTAL, 25, true, 0, 0, 0,
+                Direction.RIGHT, 1, new ArrayList<>()), 0, 0);
+            h.addObject(new OneWayRoad(Orientation.HORIZONTAL, 25, false, 0, 0, 1,
+                Direction.RIGHT, 1, new ArrayList<>()), 0, 1);
+            h.addObject(new StopSign(0, 2, new Road[4]), 0, 2);
+            h.addObject(new OneWayRoad(Orientation.HORIZONTAL, 25, false, 0, 0, 3,
+                Direction.RIGHT, 1, new ArrayList<>()), 0, 3);
+            h.addObject(new OneWayRoad(Orientation.HORIZONTAL, 25, false, 0, 0, 4,
+                Direction.RIGHT, 1, new ArrayList<>()), 0, 4);
+
+            h.addObject(new Parking(), 1, 4);
+
+            System.out.println("Grid Graph");
+            System.out.println(Arrays.deepToString(g.gridToGraph()));
+            // set up a sim
+
+            TrafficController tc1 = new TrafficController(1, h);
+
+            // run the sim
+            Simulation sim1 = tc1.runSimulation();
+
+
+
 
         } catch (InterruptedException e) {
             e.printStackTrace();
